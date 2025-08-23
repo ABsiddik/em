96e97479a5012cb831b83a85cce3fe00f216959d
Abu Bakar Siddik <absiddika@gmail.com>
2025-08-24 01:58:36 +0600

    Exception handling

---
+++ em-portal/src/main/java/itkhamar/com/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package itkhamar.com.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/employees")
+public class EmployeeController {
+}
